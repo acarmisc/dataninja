b95f426c4f9298ddf91758af6858f6a7ae685acd
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2014-03-14 15:06:15 +0100

    minor change

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+setup(name='Dataninja',
+      version='0.1',
+      description='Fetch info from different sources',
+      author='Andrea Carmisciano',
+      author_email='andrea.carmisciano@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask>=0.7.2', 'MarkupSafe'],
-     )
+      )
